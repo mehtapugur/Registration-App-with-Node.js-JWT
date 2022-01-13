87554238e0ef88d2936f8d066a3e911de4694688
Mehtap Ugur <yaziliminval@hotmail.com>
2022-01-13 03:00:16 +0300

    Add logout router

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.route("/signup").post(authController.createUser);
 router.route("/login").post(authController.loginUser);
-//router.route("/logout").get(authController.logoutUser);
+router.route("/logout").get(authController.logoutUser);
 router.route("/home").get(authMiddleware, authController.getHomePage);
 //router.route("/documents").get(authMiddleware, authController.getDocumentsPage);
 //router.route("/files").get(authMiddleware, authController.getAllFiles);
