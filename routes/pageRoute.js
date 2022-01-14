585077f805e0623cfbb4e83eaaa81144eaaf48b1
Mehtap Ugur <yaziliminval@hotmail.com>
2022-01-14 19:54:16 +0300

    Delete register routing from app

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,15 +1,10 @@
 const express = require("express");
-
 const pageController = require("../controllers/pageController");
 const redirectMiddleware = require("../middlewares/redirectMiddleware");
 const router = express.Router();
 
+//Routing
 router.route("/").get(pageController.getIndexPage);
-//router.route("/about").get(pageController.getAboutPage);
-//router.route("/register").get(redirectMiddleware, pageController.getRegisterPage);
 router.route("/login").get(redirectMiddleware, pageController.getLoginPage);
 
 module.exports = router;
-
-//tasarım için yaptım
-//router.route("/home").get(pageController.getHomePage);
