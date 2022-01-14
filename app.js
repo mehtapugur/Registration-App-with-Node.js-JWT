4e7425a07772b36f35915e2cf382e05286435dc6
Mehtap Ugur <yaziliminval@hotmail.com>
2022-01-14 19:51:50 +0300

    Use JWt and session to validation

--- app.js
+++ app.js
@@ -3,21 +3,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-// const fileUpload = require("express-fileupload");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-// const fs = require("fs");
 const path = require("path");
 const multer = require("multer"); //?
 const pageRoute = require("./routes/pageRoute");
-// //const pdfRoute = require("./routes/pdfRoute");
-// const lessonRoute = require("./routes/lessonRoute");
-// const categoryRoute = require("./routes/categoryRoute");
-// //const categoryController = require("./routes/categoryController");
 const userRoute = require("./routes/userRoute");
-// const File = require("./models/File");
 
-// //express fonksiyonunu çalıştırıyoruz
+//express fonksiyonunu çalıştırıyoruz
 const app = express();
 
 process.on("uncaughtException", (err) => {
@@ -30,8 +23,6 @@ mongoose
   .connect("mongodb://localhost/assignment-two-db", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    //useFindAndModify: false,
-    //useCreateIndex: true,
     autoIndex: true,
   })
   .then(() => {
@@ -66,7 +57,6 @@ app.use(
     }),
   })
 );
-//app.use(fileUpload());
 
 app.use(
   bodyParser.urlencoded({
@@ -74,45 +64,13 @@ app.use(
   })
 );
 
-// app.post("/api/uploadFile", (req, res) => {
-//   // Stuff to be added later
-// });
-
-// app.use("*", (req, res, next) => {
-//   userIN = req.session.userID;
-//   next();
-// });
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 
 app.use("/", pageRoute);
-// app.use("/lessons", lessonRoute);
-// app.use("/categories", categoryRoute);
-// // app.use("/pdfs", pdfRoute);
-// app.get("/about");
 app.use("/users", userRoute);
-
-// app.post("/files", async (req, res) => {
-//   console.log(req.files.pdf);
-//   const uploadDir = "./public/uploads/";
-
-//   if (fs.existsSync(uploadDir)) {
-//     fs.mkdirSync(uploadDir);
-//   }
-
-//   let uploadPdf = req.files.pdf;
-//   uploadPath = __dirname + "/public/uploads/" + uploadPdf.name;
-
-//   uploadPdf.mv(uploadPath, async () => {
-//     await File.create({
-//       ...req.body,
-//       pdf: "/uploads/" + uploadPdf.name,
-//     });
-//     //res.redirect("/");
-//     console.log("ben calistim mehom");
-//   });
-//   res.redirect("/users/files");
-// });
-
-// app.get("/files");
 
 const port = 3000;
 app.listen(port, () => {
