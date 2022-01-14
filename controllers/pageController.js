21a6739b0a29d043e98a7c9645228d2dc2c56cc7
Mehtap Ugur <yaziliminval@hotmail.com>
2022-01-14 19:56:32 +0300

    Change register and index pages

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,26 +1,9 @@
+/* Register and Login pages */
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render("index", {
     page_name: "index",
-  });
-};
-/*
-exports.getHomePage = (req, res) => {
-  res.status(200).render("home", {
-    page_name: "home",
-  });
-};
-*/
-
-exports.getAboutPage = (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-};
-
-exports.getRegisterPage = (req, res) => {
-  res.status(200).render("register", {
-    page_name: "register",
   });
 };
 
