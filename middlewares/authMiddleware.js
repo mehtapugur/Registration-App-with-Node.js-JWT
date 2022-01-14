6d060cca0d99cf180d5ea4b4e4db382142e26898
Mehtap Ugur <yaziliminval@hotmail.com>
2022-01-14 05:03:14 +0300

    Check the validation

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,29 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.cookies.token;
+
+  jwt.verify(token, req.app.get("api_secret_key"), (err, decoded) => {
+    if (err) return next();
+    req.userId = decoded.id; //? userID olsa çalışır mı
+    req.browserInfo = decoded.browserInfo;
+  });
+
+  if (
+    !(
+      req.userId == req.session.userID &&
+      req.headers["user-agent"] == req.session.browserInfo &&
+      req.headers["user-agent"] == req.browserInfo
+    )
+  )
+    return next();
+
+  next();
+};
+
+/* önceki hali
+const User = require("../models/User");
 
 module.exports = (req, res, next) => {
   User.findById(req.session.userID, (err, user) => {
@@ -6,3 +31,5 @@ module.exports = (req, res, next) => {
     next();
   });
 };
+
+*/
